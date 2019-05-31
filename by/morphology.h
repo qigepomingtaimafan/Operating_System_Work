8296f7ca3b675e20108a56b981f8657b8d2d7781
abc944813755 <944813755@qq.com>
2019-05-31 17:33:26 +0800

    2019-05-31 17:33

--- by/morphology.h
+++ by/morphology.h
@@ -7,45 +7,45 @@ using std::string;
 using std::map;
 using std::ofstream;
 //-----------------------------------------
-class Word
+class Word //Single word class
 {
     private:
-        string  s;
-        int val;
+        string  s; //the symbol of word
+        int val; //the kind of word(number)
     public:
         Word(int val,string s){this->s = s;this->val = val;}
         string GetS(){return s;}
         int GetVal(){return val;}
 };
 
-class Words
+class Words //The set of lots of word
 {
     private:
-        char character;
-        string token;
-        int lineNum;
-        string::iterator input; 
-        map<string,int> reserveMap;
-        map<string,int> symbolMap;
-        map<string,int> constantMap;
-        bool complete;
-        ofstream err;
+        char character; //Newest word has been read in 
+        string token; //String has been read
+        int lineNum; //Number of line now
+        string::iterator input; //Iterator of string from getline()
+        map<string,int> reserveMap; //map of Resetve word
+        map<string,int> symbolMap; //deleted
+        map<string,int> constantMap; //deleted
+        bool complete; //If there is some error in morphology complete will be false
+        ofstream err; //The err file 
     public:                
         Words(){InitReserveMap();complete = true;}
-        Word* LexAnalyze();
-        void GetChar();
-        void Getnbc();
-        void Concat();
-        bool Letter();
-        bool Digit();
-        void Retract();
-        int Symbol();
-        int Reserve();
-        int Constant();
-        void Error(int kind);
-        Word* CreateWord(int val,string s);
-        void InitReserveMap();
-        void SetInput(string &s){input = s.begin();}
+        Word* LexAnalyze(); //The mainly funcion,judge if word is right according symbol table 
+        void GetChar(); //read a letter into character
+        void Getnbc(); //read not blank word
+        void Concat(); //contact the character and token
+        bool Letter(); //judge character is a letter 
+        bool Digit(); // judge character is a digit
+        void Retract(); //set back the character to string 
+        int Symbol(); //deleted
+        int Reserve(); //get the reserve number of token
+        int Constant(); //deleted
+        void Error(int kind); //deal error 
+        Word* CreateWord(int val,string s); //make two return value to a word class
+        void InitReserveMap(); //setup reserve map
+        void SetInput(string &s){input = s.begin();} //set the itertor to the begin of input string 
         void SetLineNum(int lineNum){this->lineNum = lineNum;}
         bool IsComplete(){return complete;}
         bool SetErr(string errS);
